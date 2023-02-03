0a1b81ffa37d3d7c0929a665357bfcd8058b494e
gustavo.torres <gustavomtorres2009@hotmail.com>
2023-02-03 02:06:13 -0400

    First implementation

--- app/src/main.c
+++ app/src/main.c
@@ -6,40 +6,71 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/sensor.h>
 
+// Include for PM
+#include <esp_pm.h>
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <time.h>
+// #include <sys/time.h>
+// #include <sys/param.h>
+// #include "unity.h"
+// #include "esp_pm.h"
+// #include "esp_sleep.h"
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "freertos/semphr.h"
+// #include "esp_log.h"
+// #include "driver/timer.h"
+// #include "driver/rtc_io.h"
+// #include "soc/rtc_periph.h"
+// #include "esp_rom_sys.h"
+
+#include "esp32/clk.h"
+// #include "esp32/ulp.h"
+
+// #define MHZ (1000000)
+
+
 #include "app_version.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
+void set_cpu_freq(int freq){
+	esp_pm_config_esp32_t pm_config = {
+		.max_freq_mhz = freq
+	};
+
+	esp_pm_configure(&pm_config);
+    printf("Waiting for frequency to be set to %d MHz...\n", freq);
+    while (esp_clk_cpu_freq() / 1000000 != freq) {
+        k_sleep(K_MSEC(100));
+        printf("Frequency is %d MHz\n", esp_clk_cpu_freq() / 1000000);
+    }
+}
+
+void set_voltage(int voltage){
+	// Implementar aqui a lógica para alterar a tensão da CPU
+	// Não disponível para a ESP32
+
+	printk("dummy");
+}
+
 void main(void)
 {
-	int ret;
-	const struct device *sensor;
+	int counter = 0;
+	static const int test_freqs[] = {240, 40, 160, 240, 80, 40, 240, 40, 80, 10, 80, 20, 40};
 
-	printk("Zephyr Example Application %s\n", APP_VERSION_STR);
-
-	sensor = DEVICE_DT_GET(DT_NODELABEL(examplesensor0));
-	if (!device_is_ready(sensor)) {
-		LOG_ERR("Sensor not ready");
-		return;
-	}
+	printk("Dynamic Frequency Scaling Application %s\n", APP_VERSION_STR);
 
 	while (1) {
-		struct sensor_value val;
+		counter++;
+		int freq_index = counter%13 - 1;
 
-		ret = sensor_sample_fetch(sensor);
-		if (ret < 0) {
-			LOG_ERR("Could not fetch sample (%d)", ret);
-			return;
-		}
+		printk("Counter value: %d \t| Freq Index: %d \t Freq.: %d\n", counter, freq_index, test_freqs[freq_index]);
 
-		ret = sensor_channel_get(sensor, SENSOR_CHAN_PROX, &val);
-		if (ret < 0) {
-			LOG_ERR("Could not get sample (%d)", ret);
-			return;
-		}
-
-		printk("Sensor value: %d\n", val.val1);
+		// set_cpu_freq(test_freqs[freq_index]);
 
 		k_sleep(K_MSEC(1000));
 	}
