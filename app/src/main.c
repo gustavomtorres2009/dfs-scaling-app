f3ae8a4d8d94f1832a349a9a2ea894eee093cb1b
gustavo.torres <gustavomtorres2009@hotmail.com>
2023-02-26 22:27:39 -0400

    Add test enviromment using threads

--- app/src/main.c
+++ app/src/main.c
@@ -1,78 +1,137 @@
 /*
- * Copyright (c) 2021 Nordic Semiconductor ASA
+ * Copyright (c) 2017 Linaro Limited
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
-#include <zephyr/drivers/sensor.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+#include <zephyr/sys/__assert.h>
+#include <string.h>
 
-// Include for PM
-#include <esp_pm.h>
+/* size of stack area used by each thread */
+#define STACKSIZE 1024
 
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <time.h>
-// #include <sys/time.h>
-// #include <sys/param.h>
-// #include "unity.h"
-// #include "esp_pm.h"
-// #include "esp_sleep.h"
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/task.h"
-// #include "freertos/semphr.h"
-// #include "esp_log.h"
-// #include "driver/timer.h"
-// #include "driver/rtc_io.h"
-// #include "soc/rtc_periph.h"
-// #include "esp_rom_sys.h"
+/* scheduling priority used by each thread */
+#define PRIORITY_A 7
+#define PRIORITY_B 5
+#define PRIORITY_C 3
 
-#include "esp32/clk.h"
-// #include "esp32/ulp.h"
+#define LED0_NODE DT_ALIAS(led0)
+#define LED1_NODE DT_ALIAS(led1)
+#define LED2_NODE DT_ALIAS(led2)
 
-// #define MHZ (1000000)
+#if !DT_NODE_HAS_STATUS(LED0_NODE, okay)
+#error "Unsupported board: led0 devicetree alias is not defined"
+#endif
 
+#if !DT_NODE_HAS_STATUS(LED1_NODE, okay)
+#error "Unsupported board: led1 devicetree alias is not defined"
+#endif
 
-#include "app_version.h"
+#if !DT_NODE_HAS_STATUS(LED2_NODE, okay)
+#error "Unsupported board: led2 devicetree alias is not defined"
+#endif
 
-#include <zephyr/logging/log.h>
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
+struct printk_data_t {
+	void *fifo_reserved; /* 1st word reserved for use by fifo */
+	uint32_t led;
+	uint32_t cnt;
+};
 
-void set_cpu_freq(int freq){
-	esp_pm_config_esp32_t pm_config = {
-		.max_freq_mhz = freq
-	};
+K_FIFO_DEFINE(printk_fifo);
 
-	esp_pm_configure(&pm_config);
-    printf("Waiting for frequency to be set to %d MHz...\n", freq);
-    while (esp_clk_cpu_freq() / 1000000 != freq) {
-        k_sleep(K_MSEC(100));
-        printf("Frequency is %d MHz\n", esp_clk_cpu_freq() / 1000000);
-    }
-}
+struct led {
+	struct gpio_dt_spec spec;
+	uint8_t num;
+};
 
-void set_voltage(int voltage){
-	// Implementar aqui a lógica para alterar a tensão da CPU
-	// Não disponível para a ESP32
+static const struct led led0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0_NODE, gpios, {0}),
+	.num = 0,
+};
 
-	printk("dummy");
-}
+static const struct led led1 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED1_NODE, gpios, {0}),
+	.num = 1,
+};
 
-void main(void)
+static const struct led led2 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED2_NODE, gpios, {0}),
+	.num = 2,
+};
+
+void blink(const struct led *led, uint32_t sleep_ms, uint32_t id)
 {
-	int counter = 0;
-	static const int test_freqs[] = {240, 40, 160, 240, 80, 40, 240, 40, 80, 10, 80, 20, 40};
+	const struct gpio_dt_spec *spec = &led->spec;
+	int cnt = 0;
+	int ret;
 
-	printk("Dynamic Frequency Scaling Application %s\n", APP_VERSION_STR);
+	if (!device_is_ready(spec->port)) {
+		printk("Error: %s device is not ready\n", spec->port->name);
+		return;
+	}
+
+	ret = gpio_pin_configure_dt(spec, GPIO_OUTPUT);
+	if (ret != 0) {
+		printk("Error %d: failed to configure pin %d (LED '%d')\n",
+			ret, spec->pin, led->num);
+		return;
+	}
 
 	while (1) {
-		counter++;
-		int freq_index = counter%13 - 1;
+		gpio_pin_set(spec->port, spec->pin, cnt % 2);
 
-		printk("Counter value: %d \t| Freq Index: %d \t Freq.: %d\n", counter, freq_index, test_freqs[freq_index]);
+		struct printk_data_t tx_data = { .led = id, .cnt = cnt };
 
-		// set_cpu_freq(test_freqs[freq_index]);
+		size_t size = sizeof(struct printk_data_t);
+		char *mem_ptr = k_malloc(size);
+		__ASSERT_NO_MSG(mem_ptr != 0);
 
-		k_sleep(K_MSEC(1000));
+		memcpy(mem_ptr, &tx_data, size);
+
+		k_fifo_put(&printk_fifo, mem_ptr);
+
+		k_msleep(sleep_ms);
+		cnt++;
 	}
 }
 
+void blink0(void)
+{
+	blink(&led0, 100, 0);
+}
+
+void blink1(void)
+{
+	blink(&led1, 1000, 1);
+}
+
+void blink2(void)
+{
+	blink(&led2, 2000, 1);
+}
+
+void uart_out(void)
+{
+	while (1) {
+		struct printk_data_t *rx_data = k_fifo_get(&printk_fifo,
+							   K_FOREVER);
+		printk("Toggled led%d; counter=%d\n",
+		       rx_data->led, rx_data->cnt);
+		k_free(rx_data);
+	}
+}
+
+// Defining the three threads for the test enviromment
+
+K_THREAD_DEFINE(blink0_id, STACKSIZE, blink0, NULL, NULL, NULL,
+		PRIORITY_A, 0, 0);
+K_THREAD_DEFINE(blink1_id, STACKSIZE, blink1, NULL, NULL, NULL,
+		PRIORITY_B, 0, 0);
+K_THREAD_DEFINE(blink2_id, STACKSIZE, blink2, NULL, NULL, NULL,
+		PRIORITY_C, 0, 0);
+K_THREAD_DEFINE(uart_out_id, STACKSIZE, uart_out, NULL, NULL, NULL,
+		PRIORITY, 0, 0);
